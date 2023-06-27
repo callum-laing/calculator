b1133e1356db7bea7540cc66fb3b1f3877fb0a39
Laing91 <86000107+Laing91@users.noreply.github.com>
2023-06-27 22:59:49 +0100

    Added readme, html, css, and js files and created initial functions for calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+//function for addition
+let addNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+
+//function for subtraction
+let subNumbers = (num1, num2) => {
+  return num1 - num2;
+};
+
+//function for multiplicaion
+let multiplyNumbers = (num1, num2) => {
+  return num1 * num2;
+};
+
+//function for division
+let divideNumbers = (num1, num2) => {
+  return num1 / num2;
+};
