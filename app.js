c1798da7dc6e6088c4a3458e408126dc0efb022a
Laing91 <86000107+Laing91@users.noreply.github.com>
2023-06-29 23:57:08 +0100

    Basic version of calculator completed.

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-let firstOperand = "";
-let secondOperand = "";
+let firstOperand = null;
 let currentOperation = "";
+let secondOperand = 0;
+let result = null;
+
+function insertNumber(num) {
+  secondOperand = Number.parseFloat(secondOperand + "" + num);
+
+  render();
+}
+
+function insertDot() {
+  secondOperand = secondOperand + ".";
+  render();
+}
+
+function insertOperation(operation) {
+  if (firstOperand == null) {
+    firstOperand = secondOperand;
+    secondOperand = 0;
+  } else if (result !== null) {
+    firstOperand = result;
+    secondOperand = 0;
+    result = null;
+  }
+  currentOperation = operation;
+  render();
+}
 
 let add = (a, b) => {
   return a + b;
@@ -34,3 +59,52 @@ function operate(operator, num1, num2) {
       break;
   }
 }
+
+function insertEqual() {
+  result = operate(currentOperation, firstOperand, secondOperand);
+  render();
+}
+
+function clearCalculator() {
+  firstOperand = null;
+  currentOperation = "";
+  secondOperand = 0;
+  result = null;
+  render();
+}
+
+function render() {
+  if (firstOperand !== null) {
+    document.querySelector("#result").innerHTML =
+      firstOperand + " " + currentOperation;
+  } else {
+    document.querySelector("#result").innerHTML = "";
+  }
+  document.querySelector("#input").innerHTML = secondOperand;
+
+  if (result !== null) {
+    document.querySelector("#result").innerHTML =
+      firstOperand + " " + currentOperation + " " + secondOperand + " " + "=";
+    document.querySelector("#input").innerHTML = result;
+  }
+}
+
+render();
+
+/* 
+Testing:
+
+insertNumber(7);
+insertNumber(9);
+insertDot();
+insertNumber(5);
+insertOperation("+");
+insertOperation("-");
+insertNumber(6);
+insertNumber(9);
+insertEqual();
+insertOperation("+");
+insertNumber(2);
+insertEqual();
+clearCalculator();
+ */
